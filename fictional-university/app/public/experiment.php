b6b3c68f70e47395ffac05d248fc3a3797d2fb07
mackenzie willard <mtwillard29@gmail.com>
2025-01-07 19:08:29 -0500

    Using a variable

--- fictional-university/app/public/experiment.php
+++ fictional-university/app/public/experiment.php
@@ -1,3 +1,6 @@
-<?php echo 2+2 ?>
-<h1>This Page is all about Brad</h1>
-<h2>All About Brad</h2>
+<?php
+echo 2+2;
+$myName = "Brad";
+?>
+<h1>This Page is all about <?php echo $myName;?></h1>
+<h2>All About <?php echo $myName;?></h2>
